e5ef7d7ea838c2206da62c4021149e6c796b2295
gabenasci <gabenascimento1@gmail.com>
2020-10-23 22:58:19 -0300

    Tabelas de frequência das 4 variáveis

--- tabelas.R
+++ tabelas.R
@@ -1,6 +1,10 @@
-dados <- read.table("~/UFSC/Estatistica/Trabalho/competidoras.txt", 
+dados <- read.table("~/UFSC/Estatistica/trabalho-estatistica/competidoras.txt", 
                       stringsAsFactors = T,
                       header=T)
-source('~/UFSC/Estatistica/Trabalho/tab_freq.R')
+source('~/UFSC/Estatistica/trabalho-estatistica/tab_freq.R')
 tab_freq(dados$EstadoDeOrigem)
+tab_freq(dados$VitoriasSemanais)
+tab_freq(dados$VezesNaEliminacao)
+tab_freq(dados$IdadeNaTemp)
+
 
