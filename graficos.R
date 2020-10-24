3a2c22beca297963ca580cf0093edb05f1f7ce37
gabenasci <gabenascimento1@gmail.com>
2020-10-24 17:03:25 -0300

    Graficos Vitorias e Eliminações

--- graficos.R
+++ graficos.R
@@ -3,3 +3,4 @@ library(ggplot2)
 dados <- read.table("~/UFSC/Estatistica/trabalho-estatistica/competidoras.txt", 
                              stringsAsFactors = T,
                              header=T)
+
