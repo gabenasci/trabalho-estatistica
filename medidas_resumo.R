ac282ca147bab79ca1615fe4eaa62af453a90c04
gabenasci <gabenascimento1@gmail.com>
2020-10-25 16:55:38 -0300

    moda das variáveis

--- medidas_resumo.R
+++ medidas_resumo.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(e1071)
+source('~/UFSC/Estatistica/trabalho-estatistica/moda.R')
 
 dados <- read.table("~/UFSC/Estatistica/trabalho-estatistica/competidoras.txt", 
                     stringsAsFactors = T,
@@ -9,6 +10,7 @@ quantile(dados$IdadeNaTemp, c(.25, .75)) # cálculo dos percentis de ordem 25 e 
 summary(dados$IdadeNaTemp) # resumo de medidas
 mean(dados$IdadeNaTemp) # média
 median(dados$IdadeNaTemp) # mediana
+moda(dados$IdadeNaTemp) # moda
 var(dados$IdadeNaTemp) # variância
 sd(dados$IdadeNaTemp) # desvio padrão
 sd(dados$IdadeNaTemp)/mean(dados$IdadeNaTemp)*100 # coeficiente de variação
@@ -19,6 +21,7 @@ kurtosis(dados$IdadeNaTemp) # curtose
 summary(dados$VitoriasSemanais)
 mean(dados$VitoriasSemanais) # média
 median(dados$VitoriasSemanais) # mediana
+moda(dados$VitoriasSemanais) # moda
 var(dados$VitoriasSemanais) # variância
 sd(dados$VitoriasSemanais) # desvio padrão
 sd(dados$VitoriasSemanais)/mean(dados$VitoriasSemanais)*100 # coeficiente de variação
@@ -29,6 +32,7 @@ kurtosis(dados$VitoriasSemanais) # curtose
 summary(dados$VezesNaEliminacao)
 mean(dados$VezesNaEliminacao) # média
 median(dados$VezesNaEliminacao) # mediana
+moda(dados$VezesNaEliminacao) # moda
 var(dados$VezesNaEliminacao) # variância
 sd(dados$VezesNaEliminacao) # desvio padrão
 sd(dados$VezesNaEliminacao)/mean(dados$VezesNaEliminacao)*100 # coeficiente de variação
